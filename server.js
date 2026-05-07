200fe12e37529aa3be1d54c1ea24eaf84c838b56
krunalkadu <krunalkadu0507@gmail.com>
2026-05-07 22:14:51 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.static('kadu'));
+
+// app.listen(PORT,()=>{
+//    console.log(`server running at http://localhost:${PORT}`);
+// });
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const app =express();
+const PORT=3000;
 
 app.use(express.static('kadu'));
 
 app.listen(PORT,()=>{
-   console.log(`server running at http://localhost:${PORT}`);
+   console.log(`server is running at htpp://localhost:${PORT}`);
+
 });
