6c3d8ac0d0bfb8fa872da261d15a33233495cd29
krunal <krunalkadu6666@gmail.com>
2026-05-07 12:04:35 +0530

    first commit

---
+++ kadu/sta.js
@@ -0,0 +1,1 @@
+console.log("Form Loaded Successfully");
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('kadu'));
+
+app.listen(PORT,()=>{
+   console.log(`server running at http://localhost:${PORT}`);
+});
